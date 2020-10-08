579a6edbe697bc0a93aaf19afa5a0c0b80561111
JeffreyThompson <jeffathomp@gmail.com>
2020-10-08 14:51:23 -0500

    fixes connection to static files

--- main_app/views.py
+++ main_app/views.py
@@ -5,10 +5,10 @@ from .models import City
 
 def home(request): 
   context = {'title': 'Wayfarer'}
-  return render(request, 'home.html')
+  return render(request, 'home.html', context)
 
 # city routes
-def city_show(request):
+def city_show(request , city_id):
   city = City.objects.get(id = city_id)
   context = {'city': city, 'title': city.name}
   return render(request, 'cities/show.html', context)
