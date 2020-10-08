036bac302f002ae735c44cf14f9515c296a863f2
JeffreyThompson <jeffathomp@gmail.com>
2020-10-08 14:24:55 -0500

    city show route

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
+from .models import City
 
 # Create your views here.
 
 def home(request): 
+  context = {'title': 'Wayfarer'}
   return render(request, 'home.html')
+
+# city routes
+def city_show(request):
+  city = City.objects.get(id = city_id)
+  context = {'city': city, 'title': city.name}
+  return render(request, 'cities/show.html', context)
