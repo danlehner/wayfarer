f39472bbf6a4cfbf776db884f416e21d86a54529
danlehner <lehnerdr@gmail.com>
2020-10-09 13:07:32 -0400

    adds profile form and imports to forms.py

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,3 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 from .models import City, Profile, Post
 
+class Profile_Form(ModelForm):
+  class Meta: 
+    model = Profile
+    fields = ['name', 'current_city']
