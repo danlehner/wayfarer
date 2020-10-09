f6fac8f9db65cc80866ac586f3fcc0344618260b
danlehner <lehnerdr@gmail.com>
2020-10-09 10:51:42 -0400

    adds profile show page and routing

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
   path('posts/<int:post_id>/', views.post_show, name='post_show'),
   path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
   path('posts/<int:post_id>/delete/', views.post_delete, name='post_delete'),
+
+  # ==== PROFILE ==== #
+  path('profile/<int:profile_id>/', views.profile_show, name='profile_show'),
+  path('profile/<int:profile_id>/edit', views.profile_edit, name='profile_edit')
 ]
