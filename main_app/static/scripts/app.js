ac8f036551c46ec4cbb13dbbd6ec94c2b427cc7c
JeffreyThompson <jeffathomp@gmail.com>
2020-10-09 19:16:51 -0500

    upload widget functional

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -14,3 +14,15 @@ $('#app_signup').on('click', function(){
     $('.signup.modal').modal('show');
 });
 
+var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'dvk80uh1a', 
+    uploadPreset: 'fave3ym3'}, (error, result) => { 
+        if (!error && result && result.event === "success") { 
+            console.log('Done! Here is the image info: ', result.info); 
+        }
+    }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+}, false);
