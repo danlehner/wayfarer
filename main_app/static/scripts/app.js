b1acf0cf2c0efc359bd9fe54af1e1a8198f660ad
JeffreyThompson <jeffathomp@gmail.com>
2020-10-09 23:06:07 -0500

    incomplete profile image upload

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -16,7 +16,8 @@ $('#app_signup').on('click', function(){
 
 var myWidget = cloudinary.createUploadWidget({
     cloudName: 'dvk80uh1a', 
-    uploadPreset: 'fave3ym3'}, (error, result) => { 
+    uploadPreset: 'fave3ym3',
+    public_id: (document.getElementById("upload_widget").value)}, (error, result) => { 
         if (!error && result && result.event === "success") { 
             console.log('Done! Here is the image info: ', result.info); 
         }
