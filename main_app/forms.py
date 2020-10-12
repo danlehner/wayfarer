c471184e91b87379348b05500cb351e5df1429b7
jrandersen-ga <andersen.ja@gmail.com>
2020-10-12 17:43:03 -0400

    cleans up code

--- main_app/forms.py
+++ main_app/forms.py
@@ -12,15 +12,8 @@ class Post_Form(ModelForm):
     model = Post
     fields = ['title', 'text', 'city']
 
-<<<<<<< HEAD
+
 class Comment_Form(ModelForm): 
   class Meta: 
     model = Comment
     fields = ['text']
-    
-=======
-class Post_Form_Modal(ModelForm):
-  class Meta:
-    model = Post
-    fields = ['title', 'text']
->>>>>>> 6a6a791fc4d81e403e705f20ef1f5cc4f2ef1f38
